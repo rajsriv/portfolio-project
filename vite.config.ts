1e9f752149cc8af9a16149943b7cbdade1705d63
Raj Srivastava <raj2005sriv@gmail.com>
2025-07-01 09:43:25 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'portfolio-project', // Replace with your actual repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
